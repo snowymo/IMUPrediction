39be2bc407e48502a8819ed566385aee098dc21e
Zhenyi He <snowymohehe@gmail.com>
2019-01-26 03:04:32 -0500

    send rotation result by calculating accumulated gyro for validation

--- app/src/main/java/com/example/test1/BGService.java
+++ app/src/main/java/com/example/test1/BGService.java
@@ -2,6 +2,7 @@ package com.example.test1;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Matrix;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -19,6 +20,8 @@ import java.util.TimerTask;
 
 import androidx.annotation.Nullable;
 
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
 import static java.lang.Math.sqrt;
 
 public class BGService extends Service implements SensorEventListener {
@@ -27,7 +30,7 @@ public class BGService extends Service implements SensorEventListener {
     private SensorManager SM;
     private double timestamp;// in ms
 
-    private static final double SEND_RATE = 1.0 / 400.0;
+    private static final double SEND_RATE = 1.0 / 500.0;
 
     private float[] data;
 
@@ -61,6 +64,7 @@ public class BGService extends Service implements SensorEventListener {
         super.onDestroy();
     }
 
+
     @Override
     public void onSensorChanged(SensorEvent event) {
 
@@ -69,6 +73,11 @@ public class BGService extends Service implements SensorEventListener {
                 data[0] = event.values[0];
                 data[1] = event.values[1];
                 data[2] = event.values[2];
+                calculateRotationByGyro(event);
+                float[] f= new float[9];
+                rotationCurrent.getValues(f);
+                for(int i = 0; i < 9; i++)
+                    data[i+8] = f[i];
                 //Log.d("service", "onSensorChanged");
             } else if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
                 data[3] = event.values[0];
@@ -87,7 +96,64 @@ public class BGService extends Service implements SensorEventListener {
 //                Matrix2Quaternion(mRotationMatrix);
             }
         }
-        data[7] = (float) event.timestamp / 1000000000;
+        data[7] = (float) event.timestamp / 1000000;// in ms
+    }
+
+    private static final float NS2S = 1.0f / 1000000000.0f;
+    private final float[] deltaRotationVector = new float[4];
+    Matrix rotationCurrent = new Matrix();
+    void calculateRotationByGyro(SensorEvent event){
+        if (timestamp != 0) {
+            final float dT = (float) ((event.timestamp - timestamp) * NS2S);
+            // Axis of the rotation sample, not normalized yet.
+            float axisX = event.values[0];
+            float axisY = event.values[1];
+            float axisZ = event.values[2];
+
+            // Calculate the angular speed of the sample
+            double omegaMagnitude = sqrt(axisX*axisX + axisY*axisY + axisZ*axisZ);
+
+            // Normalize the rotation vector if it's big enough to get the axis
+            // (that is, EPSILON should represent your maximum allowable margin of error)
+//            if (omegaMagnitude > EPSILON) {
+//                axisX /= omegaMagnitude;
+//                axisY /= omegaMagnitude;
+//                axisZ /= omegaMagnitude;
+//            }
+
+            // Integrate around this axis with the angular speed by the timestep
+            // in order to get a delta rotation from this sample over the timestep
+            // We will convert this axis-angle representation of the delta rotation
+            // into a quaternion before turning it into the rotation matrix.
+            float thetaOverTwo = (float) (omegaMagnitude * dT / 2.0f);
+            float sinThetaOverTwo = (float) sin(thetaOverTwo);
+            float cosThetaOverTwo = (float) cos(thetaOverTwo);
+            deltaRotationVector[0] = sinThetaOverTwo * axisX;
+            deltaRotationVector[1] = sinThetaOverTwo * axisY;
+            deltaRotationVector[2] = sinThetaOverTwo * axisZ;
+            deltaRotationVector[3] = cosThetaOverTwo;
+        }
+        timestamp = event.timestamp;
+        float[] deltaRotationMatrix = new float[9];
+        SensorManager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
+        // User code should concatenate the delta rotation we computed with the current rotation
+        // in order to get the updated rotation.
+        Matrix deltaMatrix = new Matrix();
+        deltaMatrix.setValues(deltaRotationMatrix);
+        rotateMatrix(rotationCurrent, deltaMatrix);
+        Log.d("rotationCurrent", String.valueOf(rotationCurrent));
+    }
+
+    void rotateMatrix(Matrix m1, Matrix m2){
+        float[] f1 = new float[9], f2 = new float[9], f3 = new float[9];
+        m1.getValues(f1);
+        m2.getValues(f2);
+        for(int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                f3[i*3+j] = f1[i*3]*f2[j] + f1[i*3+1]*f2[1*3+j] + f1[i*3+2]*f2[2*3+j];
+            }
+        }
+        m1.setValues(f3);
     }
 
     @Override
@@ -133,7 +199,7 @@ public class BGService extends Service implements SensorEventListener {
     @Override
     public void onCreate(){
         Log.d("service", "onCreate");
-        data = new float[8];
+        data = new float[8+9];
         isStop = false;
         // Create our Sensor Manager
         SM = (SensorManager) getSystemService(SENSOR_SERVICE);
